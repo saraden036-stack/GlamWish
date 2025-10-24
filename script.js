b3f60d55bbb0aa882a384f7ae852ef2921ebb7c7
saraden036-stack <saraden036@gmail.com>
2025-10-24 08:18:21 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Animación suave al hacer clic en el menú
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
